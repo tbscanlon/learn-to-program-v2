b222d87fd31f549dffc0e7dbf34ac3fe21175ce3
tbscanlon <thomas.scanlon@outlook.com>
2017-03-22 18:36:02 +0000

    Complete most of Chapter 1

---
+++ chapter-1/cp1.rb
@@ -0,0 +1,31 @@
+puts 1 + 2
+
+# Integers
+5
+-205
+999999999999999999
+0
+
+# Floats
+54.231
+0.001
+-205.3884
+0.0
+
+# Simple Arithmetic
+
+# Floats
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+# Integers with no spaces
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2 # integer division always rounds down
+
+# More complex arithmetic
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -52
---
+++ chapter-1/ex1.rb
@@ -0,0 +1,13 @@
+# How many hours in a year
+hours_in_a_day = 24
+days_in_a_year = 365
+
+hours_in_a_year = hours_in_a_day * days_in_a_year
+puts hours_in_a_year
+
+# Minutes in a decade
+# Minutes in a year * 10
+minutes_in_a_day = 60 * 24
+minutes_in_a_year = minutes_in_a_day * 365
+minutes_in_a_decade = minutes_in_a_year * 10
+puts minutes_in_a_decade
