b9a6560bf7e08e0c9dae806776de83712573f84d
tbscanlon <thomas.scanlon@outlook.com>
2017-03-23 17:47:25 +0000

    Complete up to end of chapter 3

--- chapter-1/ex1.rb
+++ chapter-1/ex1.rb
@@ -11,3 +11,13 @@ minutes_in_a_day = 60 * 24
 minutes_in_a_year = minutes_in_a_day * 365
 minutes_in_a_decade = minutes_in_a_year * 10
 puts minutes_in_a_decade
+
+#Age in seconds
+minutes_in_a_year *= 60
+puts minutes_in_a_year * 25
+
+# How old am I?
+# 1246 million seconds
+age_in_seconds = 1_246_000_000
+
+puts age_in_seconds / minutes_in_a_year # 39 years old
---
+++ chapter-2/cp2.rb
@@ -0,0 +1,38 @@
+# Some strings
+'Hello.'
+'Ruby rocks.'
+'5 is my favourite number... what is yours?'
+'Snoopy says #%^?&*@! when he stubs his toe.'
+'     '
+''
+
+# Outputting strings
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+# Strings of my own
+puts "FIrst string."
+puts "Second string."
+
+# String Arithmetic
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie' # Adding a space in first string
+puts 'I like' + ' apple pie' # Adding a space in second string
+
+puts 'blink ' * 4
+
+# The difference between numbers and digits
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+# Escape characters
+puts 'You\'re swell!'
+puts "backslash at the end of a string: \\"
+puts 'up\\down'
+puts 'up\down'
